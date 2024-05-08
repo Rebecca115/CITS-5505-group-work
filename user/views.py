1b9970c7df876f0c8817349252b89a89f909e1ef
qinscode <fudong.qin@outlook.com>
2024-05-08 10:40:53 +0800

    update sample html, better design

--- user/views.py
+++ user/views.py
@@ -1,357 +1,214 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
-from flask_login import login_required, current_user
-from sqlalchemy import or_
+from flask import Blueprint, render_template, flash, redirect, url_for, request, jsonify, current_app
+from flask_login import logout_user, login_required, current_user
+from itsdangerous import SignatureExpired, BadSignature, URLSafeTimedSerializer
+from sqlalchemy import func
+from user.forms import RegisterForm, LoginForm, RestPassForm, ForgotPassForm
+from models import User, db, Task, Answer
 
-from models import Task, Answer, db, AnswerLike
-from task.form import WriteTaskForm, WriteAnswerForm
-
-quest = Blueprint('task', __name__,
-                  template_folder='templates',
-                  static_folder='../static')
+user = Blueprint('user', __name__, template_folder='templates', static_folder='../assets')
 
 
+@user.route('/login', methods=['GET', 'POST'])
+def login():
+    """Route for logging in the user"""
+    form = LoginForm()
 
-@quest.route('/')
-def index_page():
-    """ Home page route, display a list of paginated tasks. """
-    per_page = 5
-    page = request.args.get('page', 1, type=int)
-    page_data = Task.query.order_by(Task.created_at).paginate(
-        page=page, per_page=per_page)
-    return render_template('index.html', page_data=page_data)
+    # Process the form submission
+    if form.validate_on_submit():
+        user = form.do_login()
+        if user:
+            # Successful login
+            return redirect("/")
+        else:
+            # Login failed, show message
+            flash('Login failed, please try again.', 'danger')
+    else:
+        # Form validation failed, show message
+        flash('Login failed, please try again.', 'danger')
+
+    # Render the login template on GET or failed form submission
+    return render_template('login.html', form=form)
 
 
-@quest.route('/post', methods=['GET', 'POST'])
+# Route for handling logout
+@user.route('/logout')
 @login_required
-def post():
-    """ Route to post a new task, requires user to be logged in. """
-    form = WriteTaskForm()
+def logout():
+    """Route for logging out the user"""
+    logout_user()  # Logout the current user
+    flash('You have been logged out.', 'success')
+    return redirect("/")
 
+
+# Route for handling registration
+@user.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    # Process the form submission
     if form.validate_on_submit():
-        try:
-            que_obj = form.save()
-            if que_obj:
-                flash('Quest has been posted successfully', 'success')
-                return redirect(url_for('task.indexPage'))
-        except Exception as e:
-            flash('Error posting Quest: {}'.format(e), 'danger')
-    return render_template('post.html', form=form)
+        user_obj = form.register()
 
+        if user_obj:
+            # Registration successful
 
-@quest.route('/task/list')
-def task_list():
-    """ Route to list tasks in a paginated manner and return as JSON. """
-    try:
-        per_page = 5  # Define the number of items per page.
-        page = request.args.get('page', 1, type=int)
-        page_data = Task.query.paginate(page=page, per_page=per_page)
-        data = render_template('qa_list.html', page_data=page_data)
-        return jsonify(code=0, data=data)
-    except Exception as e:
-        return jsonify(code=1, data=str(e)), 400
+            flash('Registration successful!', 'success')
 
-
-@quest.route('/detail/<int:t_id>', methods=['GET', 'POST'])
-def detail(t_id):
-    """ Route for task details and posting answers to the task. """
-    task = Task.query.get_or_404(t_id)
-
-    answers = Answer.query.filter_by(t_id=t_id).order_by(Answer.created_at.desc()).all()
-
-    # Check if the answer is liked by the current user
-    if not current_user.is_anonymous:
-        for ans in answers:
-            ans.already_liked = AnswerLike.query.filter_by(
-                user_id=current_user.id,
-                answer_id=ans.id
-            ).first() is not None
-
-    form = WriteAnswerForm()
-
-    if form.validate_on_submit():
-        if not current_user.is_authenticated:
-            flash('Please login', 'danger')
             return redirect(url_for('user.login'))
-        try:
-            form.save(task=task)
-            flash('Answer posted successfully', 'success')
-            return redirect(url_for('task.detail', t_id=t_id))
-        except Exception as e:
-            flash('Error posting answer: {}'.format(e), 'danger')
+        else:
+            # Registration failed
+            flash('Registration failed, please try again.', 'danger')
+    else:
+        # Form errors handling
+        for fieldName, errorMessages in form.errors.items():
+            for err in errorMessages:
+                flash(f'Error in {fieldName}: {err}', 'danger')
 
-    return render_template('detail.html',
-                           task=task,
-                           answers=answers,
-                           form=form)
+    # Render the registration template on GET or failed form submission
+    return render_template('register.html', form=form)
 
 
-@quest.route('/answer/like/<int:answer_id>', methods=['POST'])
+@user.route('/<int:id>/mine')
 @login_required
-def answer_like(answer_id):
-    """ Route to like an answer, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
-
-    try:
-        # Check for existing like
-        existing_like = AnswerLike.query.filter_by(user_id=current_user.id,
-                                                   answer_id=answer_id).first()
-        if existing_like:
-            return jsonify({'error': 'You have already liked this answer'}), 409
-
-        # Create new like
-        new_like = AnswerLike(user_id=current_user.id, answer_id=answer_id)
-        db.session.add(new_like)
-        db.session.commit()
-
-        # Fetch the new like count
-        like_count = Answer.query.get(answer_id).like_count  # Assuming like_count is a field
-        return jsonify({'message': 'Success', 'like_count': like_count}), 201
-    except Exception as e:
-        return jsonify({'error': 'Unknown error: {}'.format(e)}), 500
+def mine(id):
+    """Route for displaying user profile information"""
+    # Fetch user by ID
+    user = User.query.filter_by(id=id).first_or_404(description='User not found.')
+    # Render the user profile template
+    return render_template('mine.html', user=user)
 
 
-@quest.route('/answer/unlike/<int:answer_id>', methods=['POST'])
+@user.route('/<int:id>/tasks')
 @login_required
-def answer_unlike(answer_id):
-    """ Route to unlike an answer, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
-
+def my_tasks(id):
+    """Retrieve and display tasks posted by the current logged-in user."""
     try:
-        # Check for existing like
-        existing_like = AnswerLike.query.filter_by(user_id=current_user.id,
-                                                   answer_id=answer_id).first()
-        print(existing_like)
-        if not existing_like:
-            return jsonify({'error': 'You have not liked this answer'}), 409
+        user_id = id
 
-        # Remove the like
-        db.session.delete(existing_like)
-        db.session.commit()
-
-        # Fetch the new like count
-        like_count = Answer.query.get(answer_id).like_count # Assuming like_count is a field
-        return jsonify({'message': 'Success', 'like_count': like_count}), 200
+        tasks = Task.query.filter_by(user_id=user_id).all()
+        tasks_data = [
+            {'title': task.title, 'description': task.content, 'id': task.id}
+            for task in tasks
+        ]
+        return jsonify(tasks_data)
     except Exception as e:
-        return jsonify({'error': 'Unknown error: {}'.format(e)}), 500
+        return jsonify({'error': str(e)}), 500
 
 
-@quest.route('/answer/delete/<int:answer_id>', methods=['POST'])
-def answer_delete(answer_id):
-    """ Route to delete an answer, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
+@user.route('/change_password', methods=['POST'])
+@login_required
+def change_password():
+    """Route for changing the user's password"""
 
+    data = request.get_json()
+    current_password = data.get('current_password')
+    new_password = data.get('new_password')
+
+    # Verify and update password logic
+    if not current_user.check_password(current_password):
+        return jsonify({'error': 'Current password is incorrect.'}), 400
+
+    if current_user.check_password(new_password):
+        return jsonify({'error': 'New password cannot be the same as current password.'}), 400
+
+    current_user.set_password(new_password)
+    db.session.commit()
+
+    return jsonify({'message': 'Password updated successfully'}), 200
+
+
+@user.route('/confirm-email/<token>')
+def confirm_email(token):
+    """Route for confirming the user's email address"""
     try:
-        # Check if the answer exists
-        answer = Answer.query.get(answer_id)
-        if not answer:
-            return jsonify({'error': 'Answer not found'}), 404
+        serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        email = serializer.loads(token, salt='email-confirm', max_age=3600)
 
-        # Check if the user owns the answer
-        if answer.user_id != current_user.id:
-            return jsonify({'error': 'You do not own this answer'}), 403
+        user = User.query.filter_by(email=email).first_or_404(description='User not found.')
 
-        # Delete the answer
-        db.session.delete(answer)
-        db.session.commit()
-
-        return jsonify({'message': 'Success'}), 200
-    except Exception as e:
-        return jsonify({'error': 'Unknown error: {}'.format(e)}), 500
-
-
-@quest.route('/answer/edit/<int:answer_id>', methods=['GET', 'POST'])
-def answer_edit(answer_id):
-    """ Route to edit an answer, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
-
-    answer = Answer.query.get(answer_id)
-    if not answer:
-        return jsonify({'error': 'Answer not found'}), 404
-
-    if answer.user_id != current_user.id:
-        return jsonify({'error': 'You do not own this answer'}), 403
-
-    form = WriteAnswerForm(obj=answer)
-
-    if form.validate_on_submit():
-        try:
-            answer.content = form.content.data
+        if user.email_verified:
+            return jsonify(message="This email is already confirmed."), 200
+        else:
+            user.email_verified = True
             db.session.commit()
-            return jsonify({'message': 'Success'}), 200
-        except Exception as e:
-            return jsonify({'error': 'Unknown error: {}'.format(e)}), 500
+            return jsonify(message="You have successfully confirmed your email."), 200
 
-    return render_template('edit_answer.html', form=form, answer=answer)
+    except SignatureExpired:
+        return jsonify({'error': "The confirmation link has expired."}), 400
+    except BadSignature:
 
-
-@quest.route('/answer/<int:answer_id>')
-def answer_detail(answer_id):
-    """ Route to get details of an answer. """
-    answer = Answer.query.get(answer_id)
-    if not answer:
-        return jsonify({'error': 'Answer not found'}), 404
-
-    return jsonify({'message': 'Success', 'data': answer.to_dict()}), 200
+        return jsonify({'error': "Invalid confirmation link."}), 400
 
 
-@quest.route('/answer/list/<int:t_id>')
-def answer_list(t_id):
-    """ Route to list answers to a task. """
-    answers = Answer.query.filter_by(t_id=t_id).all()
-    return jsonify({'message': 'Success', 'data': [ans.to_dict() for ans in answers]}), 200
-
-
-# @quest.route('/answer/like/list/<int:answer_id>')
-# def answer_like_list(answer_id):
-#     """ Route to list users who liked an answer. """
-#     likes = AnswerLike.query.filter_by(answer_id=answer_id).all()
-#     return jsonify({'message': 'Success', 'data': [like.to_dict() for like in likes]}), 200
-
-
-@quest.route('/search', methods=['GET'])
-def search():
-    """ Route to globally search within all task titles, task contents, and all answers. """
-    query = request.args.get('query', '')
-
-    if not query:
-        return jsonify({'error': 'Empty search query'}), 400
-
-    try:
-        matched_tasks = Task.query.filter(
-            or_(
-                Task.title.ilike(f'%{query}%'),
-                Task.content.ilike(f'%{query}%')
-            )
-        ).all()
-
-        matched_answers = Answer.query.filter(
-            Answer.content.ilike(f'%{query}%')
-        ).all()
-
-        task_results = [
-            {
-                'type': 'task',
-                'id': task.id,
-                'title': task.title,
-                'content': task.content
-            }
-            for task in matched_tasks
-        ]
-
-        answer_results = [
-            {
-                'type': 'answer',
-                'id': answer.id,
-                'content': answer.content,
-                'task_id': answer.t_id
-            }
-            for answer in matched_answers
-        ]
-
-        results = task_results + answer_results
-
-        return jsonify(results), 200
-    except Exception as e:
-        return jsonify({'error': f'Unknown error: {e}'}), 500
-
-
-@quest.route('/task/delete/<int:t_id>', methods=['POST'])
-@login_required
-def task_delete(t_id):
-    """ Route to delete a task, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
-
-    try:
-        # Check if the task exists
-        task = Task.query.get(t_id)
-        if not task:
-            return jsonify({'error': 'Task not found'}), 404
-
-        # Check if the user owns the task
-        if task.user_id != current_user.id:
-            return jsonify({'error': 'You do not own this task'}), 403
-
-        # Delete the task
-        db.session.delete(task)
-        db.session.commit()
-
-        return jsonify({'message': 'Success'}), 200
-
-    except Exception as e:
-        return jsonify({'error': f'Unknown error: {e}'}), 500
-
-
-@quest.route('/task/edit/<int:t_id>', methods=['GET', 'POST'])
-@login_required
-def task_edit(t_id):
-    """ Route to edit a task, requires user to be logged in. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
-
-    task = Task.query.get(t_id)
-    if not task:
-        return jsonify({'error': 'Task not found'}), 404
-
-    if task.user_id != current_user.id:
-        return jsonify({'error': 'You do not own this task'}), 403
-
-    form = WriteTaskForm(obj=task)
+@user.route('/forgot-password', methods=['POST', 'GET'])
+def forgot_password():
+    """Route for handling forgot password request"""
+    form = ForgotPassForm()
     if form.validate_on_submit():
-        try:
-            form.save(task=task)
-            return jsonify({'message': 'Success'}), 200
-        except Exception as e:
-            return jsonify({'error': f'Unknown error: {e}'}), 500
+        user = form.forgot_password()
+        if user:
+            return redirect(url_for('user.login'))
+        else:
+            flash('Password reset failed, please try again.', 'danger')
+    return render_template('forget_password.html', form=form)
 
 
-@quest.route('/task/<int:t_id>')
-def task_detail(t_id):
-    """ Route to get details of a task. """
-    task = Task.query.get(t_id)
-    if not task:
-        return jsonify({'error': 'Task not found'}), 404
+@user.route('/reset-password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    """Route for handling password reset"""
+    form = RestPassForm()
+    if form.validate_on_submit():
+        user = form.reset_password(token)
+        if user:
+            return redirect(url_for('user.login'))
+        else:
+            flash('Password reset failed, please try again.', 'danger')
 
-    return jsonify({'message': 'Success', 'data': task.to_dict()}), 200
+    return render_template('reset_password.html', form=form, token=token)
 
 
-@quest.route('/task/category/<category>')
-def task_by_category(category):
-    """ Route to list tasks by category. """
-    print(category)
-    tasks = Task.query.filter_by(category=category).all()
-    return jsonify({'message': 'Success', 'data': [q.to_dict() for q in tasks]}), 200
+@user.route('/get_top_users')
+def get_top_users():
+    """Route for getting top users"""
+    top_user_ids_subquery = (db.session.query(
+        Answer.user_id,
+        func.count(Answer.id).label('answer_count'))
+                             .join(User)
+                             .group_by(Answer.user_id)
+                             .order_by(func.count(Answer.id).desc())
+                             .limit(10)
+                             .subquery())
 
-@quest.route('/task/accept/<int:t_id>', methods=['POST'])
-@login_required
-def task_accept(t_id):
-    """ Route to accept an answer to a task. """
-    if not current_user.is_authenticated:
-        return jsonify({'error': 'Please login'}), 401
+    top_users = db.session.query(
+        User,
+        top_user_ids_subquery.c.answer_count
+    ).join(
+        top_user_ids_subquery, User.id == top_user_ids_subquery.c.user_id
+    ).all()
 
-    try:
-        task = Task.query.get(t_id)
-        if not task:
-            return jsonify({'error': 'Task not found'}), 404
+    users_data = []
+    for user, answer_count in top_users:
+        user_dict = user.to_dict()
+        user_dict['answer_count'] = answer_count
+        users_data.append(user_dict)
 
-        if task.user_id != current_user.id:
-            return jsonify({'error': 'You do not own this task'}), 403
+    return jsonify({"users": users_data}), 200
 
-        answer_id = request.json.get('answer_id')
-        if not answer_id:
-            return jsonify({'error': 'Missing answer_id'}), 400
-
-        answer = Answer.query.get(answer_id)
-        if not answer:
-            return jsonify({'error': 'Answer not found'}), 404
-
-        task.accepted_user_id = answer.user_id
-        db.session.commit()
-
-        return jsonify({'message': 'Success'}), 200
-    except Exception as e:
-        return jsonify({'error': f'Unknown error: {e}'}), 500
+# @user.route('/upload/<int:id>', methods=['POST'])
+# @login_required
+# def upload(id):
+#     """Route for uploading user profile picture"""
+#     if current_user.id != id:
+#         return jsonify({'error': 'You are not authorized to perform this action'}), 403
+#     user = User.query.filter_by(id=id).first_or_404(description='User not found.')
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file part'}), 400
+#     file = request.files['file']
+#     if file.filename == '':
+#         return jsonify({'error': 'No selected file'}), 400
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+#         user.profile_picture = filename
+#         db.session.commit()
+#         return jsonify({'message': 'Profile picture uploaded successfully'}), 200
+#     return jsonify({'error': 'Invalid file type'}), 400
