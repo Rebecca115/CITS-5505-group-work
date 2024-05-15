32b72881c3531fcca471fb0447775b0b84827405
ClareUWA <23918117@student.uwa.edu.au>
2024-05-15 18:09:50 +0800

    update in my test branch views.py and delete url oldin the codes

--- user/views.py
+++ user/views.py
@@ -23,24 +23,14 @@ def login():
         else:
             # Login failed, show message
             flash('Login failed, please try again.', 'danger')
-            return render_template('login_old.html', form=form)
+            return render_template('login.html', form=form)
 
 
     # Render the login template on GET or failed form submission
-    return render_template('login_old.html', form=form)
+    return render_template('login.html', form=form)
 
 
 # Route for handling logout
-# @user.route('/logout')
-# @login_required
-# def logout():
-#     """Route for logging out the user"""
-#     logout_user()  
-#     flash('You have been logged out.', 'success')
-#     redirect_url = url_for('quest.index_page')
-#     print(redirect_url) 
-#     return redirect(url_for('quest.index_page'))
-
 @user.route('/logout')
 @login_required
 def logout():
