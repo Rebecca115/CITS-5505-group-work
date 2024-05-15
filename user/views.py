92080a0e91c56af3c03a522403bc59e758c3c956
ClareUWA <23918117@student.uwa.edu.au>
2024-05-15 16:29:26 +0800

    saved

--- user/views.py
+++ user/views.py
@@ -23,11 +23,11 @@ def login():
         else:
             # Login failed, show message
             flash('Login failed, please try again.', 'danger')
-            return render_template('login.html', form=form)
+            return render_template('login_old.html', form=form)
 
 
     # Render the login template on GET or failed form submission
-    return render_template('login.html', form=form)
+    return render_template('login_old.html', form=form)
 
 
 # Route for handling logout
@@ -239,3 +239,38 @@ def change_nickname(id):
 #         db.session.commit()
 #         return jsonify({'message': 'Profile picture uploaded successfully'}), 200
 #     return jsonify({'error': 'Invalid file type'}), 400
+
+@user.route('/<int:t_id>/answer', methods=['GET'])
+@login_required
+def task_answer(t_id):
+    """ Route to get answer by user. """
+    user_id = current_user.id
+    answers = Answer.query.filter_by(user_id=user_id).all()
+
+    if not answers:
+        return jsonify({'message': 'No answers found'}), 404
+    return jsonify({'message': 'Success', 'data': [ans.to_dict() for ans in answers]}), 200
+
+
+@user.route('/<int:t_id>/change_profile', methods=['POST'])
+@login_required
+def change_profile(id):
+    """ Route to change user profile. """
+    if id != current_user.id:
+        return jsonify({'error': 'You are not authorized to perform this action'}), 403
+
+    user_id = current_user.id
+    data = request.get_json()
+    user = User.query.filter_by(id=user_id).first_or_404(description='User not found.')
+
+    if 'username' in data:
+        user.nickname = data['username']
+    if 'email' in data:
+        user.email = data['email']
+    if 'avatar' in data:
+        user.profile_picture = data['avatar']
+    if 'gender' in data:
+        user.gender = data['gender']
+
+    db.session.commit()
+    return jsonify({'message': 'Profile updated successfully'}), 200
