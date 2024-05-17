ecc81b1db109f5b6ec1094a28ff5d75af42684e1
qinscode <fudong.qin@outlook.com>
2024-05-17 20:07:18 +0800

    add new run method

--- run.py
+++ run.py
@@ -1,43 +1,49 @@
 import os
+
 from flask import Flask
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from itsdangerous import URLSafeTimedSerializer
+
 from app.task.views import quest
 from models import db, User
 from app.user.views import user
 from utils import initialize_database
 
-def create_app():
-    app = Flask(__name__, static_folder='../static', static_url_path='/../static')
-    app.config.from_object('app.conf.Config')
+app = Flask(__name__, static_folder='/app/static', static_url_path='/../static')
+app.config.from_object('app.conf.Config')
 
-    ckeditor = CKEditor(app)
-    db.init_app(app)
-    migrate = Migrate(app, db)
-    mail = Mail(app)
-    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+ckeditor = CKEditor()
+ckeditor.init_app(app)
 
-    app.register_blueprint(quest, url_prefix='/')
-    app.register_blueprint(user, url_prefix='/user')
+# init database
+db.init_app(app)
+migrate = Migrate(app, db)
 
-    login_manager = LoginManager(app)
-    login_manager.login_view = "user.login"
-    login_manager.login_message = 'Please login'
-    login_manager.login_message_category = "danger"
+mail = Mail(app)
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(user_id)
+app.register_blueprint(quest, url_prefix='/')
+app.register_blueprint(user, url_prefix='/user')
 
-    if not os.path.exists('instance/data.db'):
-        initialize_database.create_local_db(app, db)
+# Login
+login_manager = LoginManager()
+login_manager.login_view = "user.login"
+login_manager.login_message = 'Please login'
+login_manager.login_message_category = "danger"
+login_manager.init_app(app)
 
-    return app
 
-app = create_app()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+if not os.path.exists('instance/data.db'):
+    initialize_database.create_local_db(app,db)
 
 if __name__ == '__main__':
+
     app.run(host="0.0.0.0", port=8080, debug=True)
+
